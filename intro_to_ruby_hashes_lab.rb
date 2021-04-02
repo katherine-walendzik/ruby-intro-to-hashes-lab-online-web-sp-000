712c3e35503dbc5e38f97149c24e48304b1a3051
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-02 17:42:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,6 +46,12 @@ def monopoly_with_fourth_tier
 	monopoly = {
 	  railroads: {
 	    pieces: 4,
+	    rent_in_dollars: {
+	      one_piece_owned: 25,
+	      two_pieces_owned: 50,
+	      three_pieces_owned: 100,
+	      four_pieces_owned: 200
+	    },
 	    names: {
 	      reading_railroad: {
 	        "mortgage value" => "$100"
@@ -59,12 +65,6 @@ def monopoly_with_fourth_tier
 	      shortline: {
 	        "mortgage value" => "$800"
 	      }
-	    },
-	    rent_in_dollars: {
-	      one_piece_owned: 25,
-	      two_pieces_owned: 50,
-	      three_pieces_owned: 100,
-	      four_pieces_owned: 200
 	    }
 	  }
 	}
