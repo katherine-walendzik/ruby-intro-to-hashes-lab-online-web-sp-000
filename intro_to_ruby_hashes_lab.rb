c924e8020dd695af8a00ace3c9dfb669c572fe06
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-02 17:24:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,10 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {},
-	    rent_in_dollars: {}
+	    rent_in_dollars: {
+	      one_piece_owned: 25,
+	      two_piece_owned: 
+	    }
 	  }
 	}
 end
