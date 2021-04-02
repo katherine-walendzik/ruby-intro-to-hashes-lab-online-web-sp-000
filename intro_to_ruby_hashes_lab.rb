3c7a5b064a6d7737a54dceb92a3bb4fb72a80ff1
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-02 17:53:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,15 +43,15 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {
-	  railroads: {
-	    pieces: 4,
-	    rent_in_dollars: {
-	      one_piece_owned: 25,
-	      two_pieces_owned: 50,
-	      three_pieces_owned: 100,
-	      four_pieces_owned: 200
-	    },
+#	monopoly = {
+#	  railroads: {
+#	    pieces: 4,
+#	    rent_in_dollars: {
+#	      one_piece_owned: 25,
+#	      two_pieces_owned: 50,
+#	      three_pieces_owned: 100,
+#	      four_pieces_owned: 200
+#	    },
 	    names: {
 	      reading_railroad: {
 	        "mortgage value" => "$100"
